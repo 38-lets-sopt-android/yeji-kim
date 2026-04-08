362d6d628f3a5baa7f5e74c1f0ecd9ddb5fb1b17
apffkxhsls <gyeji975@gmail.com>
2026-04-09 02:04:58 +0900

    week1 제출

---
+++ app/src/main/java/com/example/letssopt/ui/theme/LoginActivity.kt
@@ -0,0 +1,162 @@
+package com.example.letssopt.ui.theme
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.jvm.java
+import android.graphics.Color as AndroidColor
+
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            navigationBarStyle = SystemBarStyle.light(
+                AndroidColor.WHITE, AndroidColor.WHITE
+            )
+        )
+        setContent {
+            LETSSOPTTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting2(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .padding(horizontal = 20.dp)
+    ){
+        var mail by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("")}
+        var realmail by remember { mutableStateOf("") }
+        var realpassword by remember { mutableStateOf("")}
+        val context = LocalContext.current
+        val signUpLauncher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                realmail = result.data?.getStringExtra("mail") ?: ""
+                realpassword = result.data?.getStringExtra("password") ?: ""
+                mail = realmail
+                password = realpassword
+            }
+        }
+
+        Text(text = "watcha", fontSize = 36.sp, color = Color(0xFFE8003C), fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 60.dp)
+                .align(Alignment.CenterHorizontally))
+        Spacer(modifier = Modifier.height(26.dp))
+        Text(text = "이메일로 로그인", fontSize = 20.sp, color = Color(0xFFFFFFFF), fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(36.dp))
+        Text(text = "이메일", color = Color(0xFF999999))
+        Spacer(modifier = Modifier.height(3.dp))
+        TextField(value = mail,
+            onValueChange = { mail = it },
+            placeholder = {Text("이메일 주소를 입력하세요")},
+            colors = TextFieldDefaults.colors(Color(0xFF2A2A2A))
+        )
+        Spacer(modifier = Modifier.height(18.dp))
+        Text(text = "비밀번호", color = Color(0xFF999999))
+        Spacer(modifier = Modifier.height(3.dp))
+        TextField(value = password,
+            onValueChange = { password = it },
+            placeholder = {Text(text = "비밀번호를 입력하세요")},
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            colors = TextFieldDefaults.colors(Color(0xFF2A2A2A))
+
+
+        )
+        Spacer(modifier = Modifier.weight(1f))
+
+        Text(text = "아직 계정이 없으신가요? 회원가입", fontSize = 14.sp, color = Color(0xFF999999),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .clickable(onClick = {
+                    val intent = Intent(context, SignUpActivity::class.java)
+                    signUpLauncher.launch(intent)
+                }))
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick =  { if(mail == realmail && password == realpassword) {
+                val intent = Intent(context, MainActivity::class.java).apply {
+                    putExtra("mail", mail)
+                    putExtra("password", password)
+                }
+                Toast.makeText(context, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                context.startActivity(intent)
+                (context as? Activity)?.finish()
+            } else {
+                Toast.makeText(context, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            } },
+            modifier = Modifier
+                .padding(bottom = 26.dp)
+                .align(Alignment.CenterHorizontally)
+                .size(width = 320.dp, height = 52.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFE8003C)),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Text("로그인", color = Color.White)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    LETSSOPTTheme {
+        Greeting2("Android")
+    }
+}
