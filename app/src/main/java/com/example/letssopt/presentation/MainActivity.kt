7f94a90eb3db64f6abf68bbfe9193bda7b1cd19d
apffxhsls <gyeji975@gmail.com>
2026-04-25 15:10:03 +0900

    개별 화면 코드 수정

--- app/src/main/java/com/example/letssopt/presentation/MainActivity.kt
+++ app/src/main/java/com/example/letssopt/presentation/MainActivity.kt
@@ -4,23 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.letssopt.R
 import com.example.letssopt.component.BottomNavigation
 import com.example.letssopt.component.LazyList
 import com.example.letssopt.component.TopNavigation
@@ -57,53 +51,14 @@ fun MainScreen(modifier: Modifier = Modifier) {
     ) { innerPadding ->
         when (selectedTab) {
             0 -> LazyList(modifier = Modifier.padding(innerPadding))
-            1 -> Box(
-                Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding)
-            ) {
-                Text(
-                    "개별구매 화면",
-                    color = Color.White,
-                    modifier = Modifier.align(Alignment.Center)
-                )
-            }
 
-            2 -> Box(
-                Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding)
-            ) {
-                Text(
-                    "웹툰 화면",
-                    color = Color.White,
-                    modifier = Modifier.align(Alignment.Center)
-                )
-            }
+            1 -> PurchaseScreen()
 
-            3 -> Box(
-                Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding)
-            ) {
-                Text(
-                    "찾기 화면",
-                    color = Color.White,
-                    modifier = Modifier.align(Alignment.Center)
-                )
-            }
+            2 -> WebtoonScreen()
 
-            4 -> Box(
-                Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding)
-            ) {
-                Text(
-                    "보관함 화면",
-                    color = Color.White,
-                    modifier = Modifier.align(Alignment.Center)
-                )
-            }
+            3 -> SearchScreen()
+
+            4 -> StorageScreen()
 
             else -> LazyList(modifier = Modifier.padding(innerPadding))
         }
