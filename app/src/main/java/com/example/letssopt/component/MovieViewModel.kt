096a2e6ccd1da3d5314753c081a08242a6820270
apffxhsls <gyeji975@gmail.com>
2026-04-25 10:57:09 +0900

    이미지 이름 수정

--- app/src/main/java/com/example/letssopt/component/MovieViewModel.kt
+++ app/src/main/java/com/example/letssopt/component/MovieViewModel.kt
@@ -5,21 +5,21 @@ import com.example.letssopt.R
 
 class MovieViewModel : ViewModel() {
     val movies = listOf<Movie>(
-        Movie("이 사랑 통역 되나요?", R.drawable.translation),
-        Movie("프로젝트 헤일메리", R.drawable.hailmary),
-        Movie("기묘한 이야기", R.drawable.starnger_things),
-        Movie("크라임씬", R.drawable.crime),
-        Movie("파묘", R.drawable.grave),
-        Movie("왕과 사는 남자", R.drawable.king)
+        Movie("이 사랑 통역 되나요?", R.drawable.img_translation),
+        Movie("프로젝트 헤일메리", R.drawable.img_hailmary),
+        Movie("기묘한 이야기", R.drawable.img_starnger_things),
+        Movie("크라임씬", R.drawable.img_crime),
+        Movie("파묘", R.drawable.img_grave),
+        Movie("왕과 사는 남자", R.drawable.img_king)
     )
 
     val movies2 = listOf<Movie>(
-        Movie("왕과 사는 남자", R.drawable.king),
-        Movie("파묘", R.drawable.grave))
+        Movie("왕과 사는 남자", R.drawable.img_king),
+        Movie("파묘", R.drawable.img_grave))
 
     val movies3 = listOf<Movie>(
-        Movie("크라임씬", R.drawable.crime),
-        Movie("프로젝트 헤일메리", R.drawable.hailmary),
-        Movie("왕과 사는 남자", R.drawable.king)
+        Movie("크라임씬", R.drawable.img_crime),
+        Movie("프로젝트 헤일메리", R.drawable.img_hailmary),
+        Movie("왕과 사는 남자", R.drawable.img_king)
     )
 }
