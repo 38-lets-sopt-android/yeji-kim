b2604990d1fff01c3120f819ea3eed3312617c3f
apffxhsls <gyeji975@gmail.com>
2026-05-04 02:15:10 +0900

    [refactor/#7] main 패키지 생성

---
+++ app/src/main/java/com/example/letssopt/presentation/main/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.letssopt.presentation.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.letssopt.ui.theme.LETSSOPTTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LETSSOPTTheme {
+            }
+        }
+    }
+}
