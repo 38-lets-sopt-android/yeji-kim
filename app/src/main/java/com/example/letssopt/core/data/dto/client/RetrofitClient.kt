1a8d15061849e49fd6b770a6924f955d836c597e
apffxhsls <gyeji975@gmail.com>
2026-05-10 02:49:07 +0900

    [feat/#8] loggingInterceptor 추가

--- app/src/main/java/com/example/letssopt/core/data/dto/client/RetrofitClient.kt
+++ app/src/main/java/com/example/letssopt/core/data/dto/client/RetrofitClient.kt
@@ -7,6 +7,7 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
 object RetrofitClient {
@@ -14,8 +15,16 @@ object RetrofitClient {
 
     private val json = Json { ignoreUnknownKeys = true }
 
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+    }
 
     private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
         .build()
 
     private val instance: Retrofit = Retrofit.Builder()
