e853c55863402a95ad809d9d6a75043f2e670eec
apffxhsls <gyeji975@gmail.com>
2026-05-12 02:53:41 +0900

    [chore/#8] interface 이름 병경 (ApiService -> AuthService)

--- app/src/main/java/com/example/letssopt/core/data/service/AuthService.kt
+++ app/src/main/java/com/example/letssopt/core/data/service/AuthService.kt
@@ -8,7 +8,7 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
-interface ApiService {
+interface AuthService {
     @POST("api/v1/auth/signup")
     suspend fun signUp(@Body request: SignUpRequest): Response<SignUpResponse>
 
