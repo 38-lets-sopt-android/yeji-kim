56b50311712fcf1d004e30950d8a0dea7a3a9e1f
apffxhsls <gyeji975@gmail.com>
2026-05-15 01:12:43 +0900

    [refactor/#8] AuthRepositoryImpl login 함수 수정(피드백)

--- app/src/main/java/com/example/letssopt/core/data/repository/impl/AuthRepositoryImpl.kt
+++ app/src/main/java/com/example/letssopt/core/data/repository/impl/AuthRepositoryImpl.kt
@@ -1,22 +1,45 @@
 package com.example.letssopt.core.data.repository.impl
 
+import com.example.letssopt.core.data.UserPreferenceManager
+import com.example.letssopt.core.data.dto.SignInRequest
+import com.example.letssopt.core.data.dto.SignUpRequest
+import com.example.letssopt.core.data.dto.client.RetrofitClient
 import com.example.letssopt.core.data.repository.AuthRepository
+import com.example.letssopt.core.data.service.AuthService
 
-class AuthRepositoryImpl : AuthRepository {
+class AuthRepositoryImpl(
+    private val autjService: AuthService = RetrofitClient.authService,
+    private val userPreferenceManager: UserPreferenceManager
+) : AuthRepository {
 
     override suspend fun login(id: String, pw: String): Result<Unit> {
-        return if (id.isNotEmpty() && pw.isNotEmpty()) {
-            Result.success(Unit)
-        } else {
-            Result.failure(Exception("로그인 실패"))
+        return runCatching {
+            val response = autjService.signIn(SignInRequest(id, pw))
+            if (response.isSuccessful) {
+                userPreferenceManager.saveUserLoginState(true)
+            } else {
+                throw Exception("아이디 또는 비밀번호가 틀렸습니다.")
+            }
         }
     }
 
-    override suspend fun signUp(id: String, pw: String, mail: String, name: String, age: Int, part: String): Result<Unit> {
-        return if (id.isNotEmpty() && pw.isNotEmpty() && mail.isNotEmpty() && name.isNotEmpty() && age > 0 && part.isNotEmpty()) {
-            Result.success(Unit)
-        } else {
-            Result.failure(Exception("회원가입 실패"))
+
+    override suspend fun signUp(
+        id: String,
+        pw: String,
+        mail: String,
+        name: String,
+        age: Int,
+        part: String
+    ): Result<Unit> {
+        return runCatching {
+            val response = autjService.signUp(
+                SignUpRequest(id, pw, mail, name, age, part)
+            )
+            if (response.isSuccessful) {
+            } else {
+                throw Exception(response.message() ?: "회원가입에 실패했습니다.")
+            }
         }
     }
 
